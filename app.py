972f37c78d14e29c68ec5557358d8d00426fda14
basarYargici <ibrahimbyargici@gmail.com>
2023-05-19 16:22:06 +0300

    add undefined error and code format

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import json
+import numpy as np
 from flask import Flask, request
 from flask_restful import Resource, Api
-import numpy as np
 from tensorflow.keras.models import load_model
-import json
 from tensorflow.keras.optimizers import Adam
+
 from constants import requests_count, model_path, class_names, treatment_links, response_dict, schedule
 from utils import NumpyEncoder, get_image_prediction
 
@@ -23,7 +24,6 @@ plant_resnet_model = load_model(model_path, compile=False)
 plant_resnet_model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-
 class PlantHealthAnalyse(Resource):
     def post(self, response_dict=response_dict):
         try:
@@ -32,32 +32,50 @@ class PlantHealthAnalyse(Resource):
             data = np.array(img_data)
 
             prediction = plant_resnet_model.predict(data)
-            for i in range(1,5,1):
+            for i in range(1, 5, 1):
                 response_dict["plantHealthModels"].append({
-                        "plantName": class_names[np.argsort(np.max(prediction, axis=0))[-i]].split()[0],
-                        "diseaseName": class_names[np.argsort(np.max(prediction, axis=0))[-i]],
-                        "treatmentLink": treatment_links[class_names[np.argsort(np.max(prediction, axis=0))[-i]]],
-                        "probability": prediction[0][np.argsort(np.max(prediction, axis=0))[-i]],
-                        "wateringSchedule": schedule[class_names[np.argsort(np.max(prediction, axis=0))[-i]]]["hourly"]
-                    })
+                    "plantName": class_names[np.argsort(np.max(prediction, axis=0))[-i]].split()[0],
+                    "diseaseName": class_names[np.argsort(np.max(prediction, axis=0))[-i]],
+                    "treatmentLink": treatment_links[class_names[np.argsort(np.max(prediction, axis=0))[-i]]],
+                    "probability": prediction[0][np.argsort(np.max(prediction, axis=0))[-i]],
+                    "wateringSchedule": schedule[class_names[np.argsort(np.max(prediction, axis=0))[-i]]]["hourly"]
+                })
 
             if prediction[0][np.argsort(np.max(prediction, axis=0))[-1]] < 0.5:
-                return json.dumps(response_dict, cls=NumpyEncoder)
+                return json.dumps({"error": "Undefined"})
             elif prediction[0][np.argsort(np.max(prediction, axis=0))[-1]] < 0.7:
+                # TODO: (Fatih) Check here
                 global first_try
                 first_try = requests_count["/predict"]
                 if requests_count["/predict"] - first_try >= 3:
-
                     first_try = requests_count["/predict"]
                     return json.dumps(response_dict, cls=NumpyEncoder)
-                return json.dumps({"error":"Take a closer photograph of the leaf and try again."})
+                return json.dumps({"error": "Take a closer photograph of the leaf and try again."})
             else:
                 return json.dumps(response_dict, cls=NumpyEncoder)
         except Exception as e:
             print("Error: ", str(e))
             return json.dumps({"error": str(e)})
 
+
 api.add_resource(PlantHealthAnalyse, '/plantHealthAnalyse')
+
+
+@app.route('/count')
+def count():
+    print(requests_count)
+    return requests_count
+
+
+@app.route('/analyseHealth')
+def classify():
+    query_string = request.query_string.decode('utf-8')
+    image_url = query_string[4:]
+    response = get_image_prediction(image_url, "http://127.0.0.1:5000/plantHealthAnalyse")
+    return response
+
+
+# region Api For Flower Model
 
 # flower_resnet_model = load_model(model_path, compile=False)
 # flower_resnet_model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
@@ -96,30 +114,13 @@ api.add_resource(PlantHealthAnalyse, '/plantHealthAnalyse')
 #
 # api.add_resource(FlowerClassification, '/flowerClassification')
 
-
-
-
-@app.route('/count')
-def count():
-    print(requests_count)
-    return requests_count
-
-
-@app.route('/analyseHealth')
-def classify():
-    query_string = request.query_string.decode('utf-8')
-    image_url = query_string[4:]
-    response = get_image_prediction(image_url, "http://127.0.0.1:5000/plantHealthAnalyse")
-    return response
-
 # @app.route('/classifyFlower')
 # def classifyFlower():
 #     query_string = request.query_string.decode('utf-8')
 #     image_url = query_string[4:]
 #     response = get_image_prediction(image_url, "http://127.0.0.1:5000/flowerClassification")
 #     return response
+# endregion
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 
+
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.float32):
@@ -11,19 +12,19 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
-
 def load_image_from_url(img_url):
     response = requests.get(img_url)
     img = Image.open(BytesIO(response.content))
     return img
+
 
 def preprocess_image(image, target_size=(224, 224)):
     image_resized = image.resize(target_size)
     image_array = np.array(image_resized)
     return np.expand_dims(image_array / 255., axis=0)  # Normalize and add batch dimension
 
-def get_image_prediction(image_url, api_url):
 
+def get_image_prediction(image_url, api_url):
     image = load_image_from_url(image_url)
 
     preprocessed_image = preprocess_image(image)
@@ -35,6 +36,3 @@ def get_image_prediction(image_url, api_url):
     print("API response:", response.text)  # Print the response text
     # Return the response JSON
     return response.json()
-
-
-
